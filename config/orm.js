93ec8cc8b7231fdaee1faa41c6439e927544feb7
Joel Terrell <falconreid@hotmail.com>
2020-09-18 21:28:05 -0600

    created basic files, added boilerplate code

---
+++ config/orm.js
@@ -0,0 +1,29 @@
+// const { update } = require("../models/burger.js");
+
+var connection = require("./connection.js");
+
+var orm = {
+  selectAll: function (id, burgers) {
+    var queryString = "SELECT * FROM burgers";
+    connection.query(queryString, [id, burgers], function (err, res) {
+      if (err) throw err;
+      return res;
+    });
+  },
+  insertOne: function (id, newburger) {
+    var queryString = "INSERT INTO ?? (??, ??) VALUES(?,?)";
+    connection.query(queryString, [id, newburger], function (err, res) {
+      if (err) throw err;
+      return res;
+    });
+  },
+  updateOne: function (id, eatburger) {
+    var queryString = "INSERT INTO ?? (??, ??) VALUES(?,?)";
+    connection.query(queryString, [id, eatburger], function (err, res) {
+      if (err) throw err;
+      return res;
+    });
+  },
+};
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Avocado Bacon Burger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Double Bacon Cheese Burger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Gourmet Mushroom Burger', false);
